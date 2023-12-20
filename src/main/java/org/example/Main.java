1a7a8291481646b52389ee8d0b2cdf98bed3e3bd
Zack Colton <z.colton.2022@bristol.ac.uk>
2023-12-20 13:16:56 +0000

    Switch to JDA to use a real bot account. Also show user's PFP in notification

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,24 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.example.discordObjects.Mention;
-import org.example.discordObjects.Message;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.ISnowflake;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Type;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import static java.lang.Thread.sleep;
 import static org.example.apiCalls.API.*;
 import static org.example.util.Util.OutputFormat.ERROR;
 import static org.example.util.Util.OutputFormat.STDOUT;
@@ -32,65 +35,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String lastId = "";
+        // Start the JDA, and wait for it to be ready
+        JDA jda = startBot();
 
-        // Try to get as many messages as possible up to the limit
-        List<Message> quotes = new ArrayList<>();
+        TextChannel channel = jda.getTextChannelById(ENV.get("CHANNEL_ID"));
 
-        // Get the maximum number of quotes we need
-        int maxQuotes = Integer.parseInt(ENV.get("MAX_QUOTES"));
-
-        // Keep getting more quotes until we have enough
-        while (quotes.size() < maxQuotes) {
-
-            try {
-                // Sleep to avoid rate limits
-                //noinspection BusyWait
-                sleep(1000);
-            } catch (InterruptedException e) {
-                printOutput("Thread was interrupted whilst trying to sleep: " +
-                        e.getMessage() + "\nContinuing...", ERROR);
-            }
-
-            // Get a list of the next 50 messages
-            List<Message> nextMessages = getListOfMessages(lastId);
-
-            // If the set of nextMessages is empty, it means that either we've been rate limited, or locked out
-            // of the API, so we should gracefully just stop, and use whatever messages we already have.
-            if (!nextMessages.isEmpty()) {
-                lastId = nextMessages.get(nextMessages.size() - 1).getId();
-
-                // Filter only messages that are quotes
-                nextMessages =
-                        nextMessages.parallelStream()
-                                .filter(x -> x.getContent().matches(FULL_QUOTE_REGEX)).toList();
-
-                // Add these to the list of quotes
-                quotes.addAll(nextMessages);
-            } else {
-                break;
-            }
-
-            printOutput(String.format("%d quotes fetched so far.", quotes.size()), STDOUT);
+        if (channel == null) {
+            printOutput(String.format("Could not get channel with ID %s. Exiting...", ENV.get("CHANNEL_ID")), ERROR);
+            System.exit(1);
         }
 
+        // Get the maximum number of quotes we need
+        int maxMessages = Integer.parseInt(ENV.get("MAX_MESSAGES"));
+
+        // Get a list of the max quotes we want
+        List<Message> messages = getListOfMessages(channel, maxMessages);
+
+        printOutput(String.format("%d messages fetched.", messages.size()), STDOUT);
+
+        // Get only those messages which follow the quote format
+        messages = messages.parallelStream().filter(x -> x.getContentRaw().matches(FULL_QUOTE_REGEX)).toList();
+
         // Filter quotes only to be those where people who've opted-in are mentioned
-        quotes = filterOptIn(quotes);
+        messages = filterOptIn(messages);
+
+        printOutput(String.format("%d quotes fetched.", messages.size()), STDOUT);
 
         // Get a random quote from the list
-        Message randQuote = quotes.get(new Random().nextInt(quotes.size()));
-
-        // "Fix" the content - i.e., just replace any raw user IDs with their server nicknames
-        randQuote = randQuote.fixContent(ENV.get("SERVER_ID"));
+        Message randQuote = messages.get(new Random().nextInt(messages.size()));
 
         // Print for logging
-        printOutput("Quote is: " + randQuote.getContent(), STDOUT);
+        printOutput("Quote is: " + cleanContent(randQuote), STDOUT);
 
         // Try to send the quote as a push notification.
         // If this is true, then the message failed to send. In that case, try to send again and if it still
         // fails, just print the error and exit.
-        if (pushMessage(randQuote, ENV.get("SERVER_ID"), ENV.get("CHANNEL_ID"))) {
-            if (pushMessage(randQuote, ENV.get("SERVER_ID"), ENV.get("CHANNEL_ID"))) {
+        if (pushMessage(randQuote)) {
+            if (pushMessage(randQuote)) {
                 printOutput("Failed to send the push notification; tried twice.", ERROR);
             } else {
                 printOutput("Successfully sent push notification on the second attempt.", STDOUT);
@@ -98,17 +79,54 @@ public class Main {
         } else {
             printOutput("Successfully sent push notification.", STDOUT);
         }
+
+        // Exit without error
+        System.exit(0);
     }
 
     /**
-     * Send a notification through PushSafer with the passed in quote message. Uses the key specified in .env
+     * Given a message, we 'clean' its content, by replacing all IDs with the server nickname of the respective user
      *
-     * @param message   The message with the quote in
-     * @param serverId  The ID of the server the message comes from
-     * @param channelId The ID of the server the message comes from
+     * @param message The message to be 'cleaned'
+     * @return The 'clean' content of the message
+     */
+    private static @NotNull String cleanContent(@NotNull Message message) {
+        String content = message.getContentRaw();
+
+        // This matches things of the form <@ID> or <@!ID>
+        String reString = "<@!?\\d{15,20}>";
+        Pattern regex = Pattern.compile(reString);
+
+        // Check there are any matches
+        if (regex.matcher(content).find()) {
+            Map<String, String> mentionsMap = new HashMap<>();
+
+            // Create a map from IDs to names. If these name can't be fetched from the server, just use their username.
+            for (User user : message.getMentions().getUsers()
+            ) {
+                mentionsMap.put(user.getId(), getServerNickname(message.getJDA(), user.getId(), Objects.requireNonNull(message.getGuildId())));
+            }
+
+            Matcher matcher = regex.matcher(content);
+
+            // Perform the replacements
+            while (matcher.find()) {
+                String group = matcher.group();
+                String id = group.replace("<@", "").replace("!", "").replace(">", "");
+                content = content.replace(group, mentionsMap.get(id));
+            }
+        }
+
+        return content;
+    }
+
+    /**
+     * Send a notification through NTFY with the passed in quote message. Uses the key specified in .env
+     *
+     * @param message The message with the quote in
      * @return A boolean, true if an error occurred, false otherwise
      */
-    private static boolean pushMessage(Message message, String serverId, String channelId) {
+    private static boolean pushMessage(Message message) {
 
         try {
             // Form the URL from the NTFY URL
@@ -121,18 +139,23 @@ public class Main {
 
             // Add the content
             // Remove newlines from the title
-            conn.setRequestProperty("Title", "\uD83D\uDD38" + message.getContent().replace("\n", "\uD83D\uDD38"));
-            conn.setRequestProperty("Click", String.format("https://discord.com/channels/%s/%s/%s", serverId,
-                    channelId, message.getId()));
+            conn.setRequestProperty("Title", "\uD83D\uDD38" + cleanContent(message).replace("\n", "\uD83D\uDD38"));
+            conn.setRequestProperty("Click", String.format("https://discord.com/channels/%s/%s/%s",
+                    message.getGuild().getId(),
+                    message.getChannelId(), message.getId()));
+
+            // Add the user's avatar as an image
+            conn.setRequestProperty("Attach", getAvatarURL(message.getMentions().getUsers().get(0)));
 
             // Write the body of the message (which in this case, is just the "Quoted by" string)
             OutputStream os = conn.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-            osw.write("Quoted by: " + message.getAuthor().getName(serverId));
+
+            // Get the name of the person who saved the quote
+            osw.write("Quoted by: " + getServerNickname(message.getJDA(), message.getAuthor().getId(), Objects.requireNonNull(message.getGuildId())));
             osw.flush();
             osw.close();
             os.close();  //don't forget to close the OutputStream
-
 
             // Make the request
             makeRequest(conn);
@@ -147,47 +170,35 @@ public class Main {
     }
 
     /**
-     * Get the most recent 50 messages in the channel specified in .env
+     * Given a User object, return the URL that points to their avatar. If it cannot be found, return a URL to the
+     * STA logo (stored in the .env file).
      *
-     * @param sinceId If this is not "", we will get all messages sent before a message with this given ID.
-     *                Otherwise, get just the most recent messages.
-     * @return A list of the 50 messages either before the message with the given ID, or just the most recent 50 in
-     * the channel.
+     * @param user The user to get the avatar of
+     * @return A string, representing the URL of the user's avatar. If it cannot be found, return a URL to the STA logo.
      */
-    private static List<Message> getListOfMessages(String sinceId) {
 
-        // Form the URL, different based on whether we have a previous message to get before or not
-        URL url;
+    private static @NotNull String getAvatarURL(@NotNull User user) {
 
-        try {
-            if (!Objects.equals(sinceId, "")) {
-                url = new URL("https://discord.com/api/v9/channels/" + ENV.get("CHANNEL_ID") + "/messages?before=" +
-                        sinceId + "&limit=50");
-            } else {
-                url = new URL("https://discord.com/api/v9/channels/" + ENV.get("CHANNEL_ID") + "/messages?limit=50");
-            }
-        } catch (MalformedURLException e) {
-            printOutput("An error occurred when trying to form the URL in getListOfMessages: " + e.getMessage(), ERROR);
-            return new ArrayList<>();
-        }
+        // Return the user's URL. If it can't be found, return a link to the STA logo
+        return Objects.requireNonNullElse(user.getAvatarUrl(), ENV.get("STA_LOGO"));
+    }
 
-        // If either of these throws an error, we'll just gracefully print out the error and return no messages
-        try {
-            // Create the connection
-            HttpURLConnection conn = createDiscordConn(url);
 
-            // Read the response as JSon
-            String response = makeRequest(conn);
+    /**
+     * Get the most recent messages in a channel as a list and return them
+     *
+     * @param channel     The channel to get the messages from
+     * @param maxMessages The maximum number of messages to fetch
+     * @return The list of messages
+     */
+    private static @NotNull List<Message> getListOfMessages(@NotNull MessageChannel channel,
+                                                            int maxMessages) {
+        // Wait for this to complete before we return
+        CompletableFuture<List<Message>> messageFutures =
+                channel.getIterableHistory().takeAsync(maxMessages);
 
-            // Parse the response as a list
-            Type listType = new TypeToken<ArrayList<Message>>() {
-            }.getType();
-            return (new Gson().fromJson(response, listType));
-        } catch (Exception e) {
-            printOutput("An error occurred when trying to make HTTP requests in getListOfMessages: " + e.getMessage(),
-                    ERROR);
-            return new ArrayList<>();
-        }
+        // Wait for all messages to be retrieved before returning
+        return messageFutures.join();
 
     }
 
@@ -204,12 +215,14 @@ public class Main {
 
         // Only take messages which mention people, because they're the easiest to filter.
         // Then, filter only those where all mentions have opted-in
-        return allMessages.parallelStream().filter(x -> !x.getMentions().isEmpty())
-                // Ensure that all mentions are in the optIns list
-                .filter(x -> new HashSet<>(optIns).containsAll(
-                        // Map each mention to their ID and collect again
-                        x.getMentions().stream().map(Mention::getId)
-                                .collect(Collectors.toSet()))).toList();
 
+        return allMessages.parallelStream()
+                // Make sure a message actually has some mentions
+                .filter(m -> !m.getMentions().getUsers().isEmpty())
+                // Make sure all mentioned people are in the opt-in list
+                .filter(m -> new HashSet<>(optIns).containsAll(
+                        m.getMentions().getUsers().stream().map(ISnowflake::getId)
+                                .collect(Collectors.toSet())))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/org/example/apiCalls/API.java
+++ src/main/java/org/example/apiCalls/API.java
@@ -1,14 +1,19 @@
 package org.example.apiCalls;
 
-import com.google.gson.Gson;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.example.discordObjects.User;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
 
 import static org.example.util.Util.OutputFormat.ERROR;
 import static org.example.util.Util.printOutput;
@@ -26,49 +31,25 @@ public class API {
      * @param serverId The server's ID
      * @return The user's nickname/global name/username
      */
-    public static @NotNull String getServerNickname(@NotNull String userId, @NotNull String serverId) {
-
+    public static @NotNull String getServerNickname(@NotNull JDA jda, @NotNull String userId, @NotNull String serverId) {
         try {
-            URL url = new URL("https://discord.com/api/v9/users/" + userId + "/profile?with_mutual_guilds=true");
 
-            HttpURLConnection conn = createDiscordConn(url);
+            // Try to get the user's server-specific nickname
+            String nickname =
+                    Objects.requireNonNull(jda.getGuildById(serverId)).retrieveMemberById(userId).complete().getNickname();
 
-            String response = makeRequest(conn);
-
-            User user = new Gson().fromJson(response, User.class);
-
-            return user.getNickname(serverId);
-        } catch (IOException e) {
-            printOutput("An error occurred when trying to send off an HTTP request in getServerNickname: " + e.getMessage(), ERROR);
+            // If the user has no server nickname, then return their global nickname instead. Failing that, get their
+            // username.
+            return Objects.requireNonNullElseGet(nickname,
+                    () -> Objects.requireNonNullElse(
+                            jda.retrieveUserById(userId).complete().getGlobalName(),
+                            jda.retrieveUserById(userId).complete().getName()));
+        } catch (NullPointerException e) {
+            printOutput(String.format("An error occurred when trying to get the name of a user with ID %s: %s", userId,
+                    e.getMessage()), ERROR);
             return "";
         }
-    }
 
-    /**
-     * Given a URL, set the GET method on it and add the Discord token from .env for authorisation. Then return it as a
-     * new
-     * HttpURLConnection
-     *
-     * @param url The URL to connect to
-     * @return An HttpURLConnection to said URL
-     */
-    public static @NotNull HttpURLConnection createDiscordConn(@NotNull URL url) throws IOException {
-        // Load the environment variables
-        Dotenv dotenv = Dotenv.configure().load();
-
-        try {
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            conn.setDoOutput(true);
-
-            // Set request headers
-            conn.setRequestProperty("Authorization", dotenv.get("DISCORD_TOKEN"));
-
-            return conn;
-        } catch (IOException e) {
-            throw new IOException("An error occurred when configuring a URL in createDiscordConn: " + e.getMessage());
-        }
     }
 
     /**
@@ -99,6 +80,9 @@ public class API {
      * @param conn The connection to make the request from
      * @return The response from the request
      */
+    
+    // It's good to keep this returning something, for the future & for the error-handling, even if we don't use it now
+    @SuppressWarnings("UnusedReturnValue")
     public static @NotNull String makeRequest(@NotNull HttpURLConnection conn) {
 
         // Read the response as JSon
@@ -117,6 +101,38 @@ public class API {
             printOutput("An error occurred when calling makeRequest: " + e.getMessage(), ERROR);
             return "";
         }
+    }
+
+
+    /**
+     * Start up the bot, using the "DISCORD_TOKEN" in the .env file
+     *
+     * @return A new JDA object, which is the instance of the bot
+     */
+    public static @NotNull JDA startBot() {
+
+        JDABuilder builder = JDABuilder.createDefault(ENV.get("DISCORD_TOKEN"));
+
+        // Set activity
+        builder.setActivity(Activity.customStatus("Stealing your quotes"));
+
+        // Set the intent to read message content
+        builder.setEnabledIntents(GatewayIntent.MESSAGE_CONTENT);
+
+        // Disable a load of stuff we don't need, to save bandwidth
+        builder.disableCache(List.of(CacheFlag.VOICE_STATE, CacheFlag.EMOJI, CacheFlag.STICKER, CacheFlag.SCHEDULED_EVENTS));
+
+        JDA jda = builder.build();
+
+        try {
+            jda.awaitReady();
+        } catch (InterruptedException e) {
+            printOutput("Thread was interrupted whilst trying to wait for JDA to be ready: " +
+                    e.getMessage() + "\nExiting...", ERROR);
+            System.exit(1);
+        }
+
+        return jda;
     }
 
 }
